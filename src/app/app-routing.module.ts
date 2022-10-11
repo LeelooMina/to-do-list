30a6244f451c6d6dc1b8546d67f59631c5cd7627
Crystal Campbell <blankpage@gmail.com>
2022-10-10 20:30:16 -0500

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ToDoFormComponent } from './to-do-form/to-do-form.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 
 const routes: Routes = [
   {
     path: 'todos',
-    component: ToDoListComponent
-  }
+    component: ToDoListComponent, children: [
+      {
+        path: 'add',
+        component: ToDoFormComponent
+      }
+
+    ]
+    }
+
 ];
 
 @NgModule({
